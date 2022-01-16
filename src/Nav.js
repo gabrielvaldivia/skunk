5a682ab04f842fc01568d404cd11865c22ce5a87
Claudio Vallejo <hola@claudiovallejo.mx>
2022-01-16 11:40:10 -0500

    Installed React Router, Added NavLinks

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 import './App.css';
 
 function Nav() {
+  const navLinkClassNames = "m-0px mr-32px ff-big-noodle fw-normal fs-60px c-white td-none o-0_5";
+
   return (
     <div className="d-flex ox-scroll ws-nowrap hide-scrollbar pt-24px ph-24px">
-      <h1 className="ff-big-noodle fw-normal fs-60px c-white m-0px mr-32px">
+      <NavLink
+        exact
+        to="/"
+        className={navLinkClassNames}
+        activeClassName="o-1"
+      >
         Smash Bros
-      </h1>
-      <h1 className="ff-big-noodle fw-normal fs-60px c-white m-0px mr-32px o-0_5">
+      </NavLink>
+      <NavLink
+        exact
+        to="/ping-pong"
+        className={navLinkClassNames}
+        activeClassName="o-1"
+      >
         Ping Pong
-      </h1>
+      </NavLink>
     </div>
   );
 }
